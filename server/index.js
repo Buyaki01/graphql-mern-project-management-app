d2cbbd6180cf66c96c1abade2685582bb81805e1
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-08 12:43:45 +0300

    Add schema for client type

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema.js');
 const port = process.env.PORT || 5500;
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: process.env.NODE_ENV === 'development'
-}))
+app.use(
+  '/graphql', 
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
