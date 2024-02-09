b5942879ec9c35fd5862e50df60053f25aa65c56
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-09 12:42:32 +0300

    Add client mutations

--- server/models/Client.js
+++ server/models/Client.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const ClientSchema = new mongoose.Schema({
   name: {
     type: String,
+    unique: true,
   },
   email: {
     type: String,
+    unique: true,
   },
   phone: {
     type: String,
