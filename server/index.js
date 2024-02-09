4e8dcf12373f85e553db8fbc7308381dde0cfe13
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-09 10:35:09 +0300

    Add Project Type to the schema, connect app to mongoose

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config({ path: '../.env' });
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema.js');
-const port = process.env.PORT || 5500;
+const connectDB = require('./config/db.js');
+const port = process.env.PORT || 7000;
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   '/graphql', 
