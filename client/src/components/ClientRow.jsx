46b3d464a5c238d9647e341678a7bf530029ad3b
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-13 10:35:37 +0300

    Add to ClientRow component, create queries and add clientQueries to it

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,17 @@
-const ClientRow = () => {
+import { FaTrash } from 'react-icons/fa'
+
+const ClientRow = ({ client }) => {
   return (
-    <div>ClientRow</div>
+    <tr>
+      <td>{ client.name }</td>
+      <td>{ client.email }</td>
+      <td>{ client.phone }</td>
+      <td>
+        <button className="btn btn-danger btn-sm">
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
   )
 }
 
